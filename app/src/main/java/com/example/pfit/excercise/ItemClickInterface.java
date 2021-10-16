270d326b44f9910718b6ff39dac92a8d206431f2
nhatm <nhatquan1702>
2021-10-16 11:39:51 +0700

    Thêm giao diện bài tập thêm fonts asetss fix chiều cao bottom sheet

---
+++ app/src/main/java/com/example/pfit/excercise/ItemClickInterface.java
@@ -0,0 +1,7 @@
+package com.example.pfit.excercise;
+
+import android.view.View;
+
+public interface ItemClickInterface {
+    void onClick(View view, int position);
+}
--- app/src/main/java/com/example/pfit/fragment/Fragment_Excercise.java
+++ app/src/main/java/com/example/pfit/fragment/Fragment_Excercise.java
@@ -10,19 +10,12 @@ import android.view.ViewGroup;
 
 import com.example.pfit.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_Excercise#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment_Excercise extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +23,6 @@ public class Fragment_Excercise extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Excercise.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment_Excercise newInstance(String param1, String param2) {
         Fragment_Excercise fragment = new Fragment_Excercise();
         Bundle args = new Bundle();
@@ -61,6 +45,7 @@ public class Fragment_Excercise extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__excercise, container, false);
+        View view = inflater.inflate(R.layout.fragment__excercise, container, false);
+        return view;
     }
 }
